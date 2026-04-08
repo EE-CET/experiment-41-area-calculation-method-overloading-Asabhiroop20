4b2f71518ddca26999825ca51cc54e0a2cf2e5b3
Asabhiroop20 <abhiroopas2423@gmail.com>
2026-04-08 14:07:29 +0530

    first commit of Method overloading

--- src/AreaCalculator.java
+++ src/AreaCalculator.java
@@ -2,20 +2,38 @@ import java.util.Scanner;
 
 public class AreaCalculator {
     
-    // TODO: Define calculateArea(float radius) -> returns area of circle
-    // Use Math.PI or 3.14159f for pi
+   // Circle
+    float calculateArea(float radius) {
+        return (float)(Math.PI * radius * radius);
+    }
 
-    // TODO: Define calculateArea(float length, float breadth) -> returns area of rectangle
+    // Rectangle
+    float calculateArea(float length, float breadth) {
+        return length * breadth;
+    }
 
-    // TODO: Define calculateArea(double base, double height) -> returns area of triangle
+    // Triangle
+    double calculateArea(double base, double height) {
+        return 0.5 * base * height;
+    }
+
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        // TODO: Read input for Circle (radius)
-        // TODO: Read input for Rectangle (length, breadth)
-        // TODO: Read input for Triangle (base, height)
-        
-        // TODO: Call methods and print results formatted to 2 decimal places
-        // Hint: Use System.out.printf("%.2f\n", area);
+       AreaCalculator obj = new AreaCalculator();
+
+        float radius = sc.nextFloat();
+        float length = sc.nextFloat();
+        float breadth = sc.nextFloat();
+        double base = sc.nextDouble();
+        double height = sc.nextDouble();
+
+        float circleArea = obj.calculateArea(radius);
+        float rectangleArea = obj.calculateArea(length, breadth);
+        double triangleArea = obj.calculateArea(base, height);
+
+        System.out.printf("%.2f\n", circleArea);
+        System.out.printf("%.2f\n", rectangleArea);
+        System.out.printf("%.2f", triangleArea);
     }
 }
